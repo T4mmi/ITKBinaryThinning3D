291074da4e41e758c963a2b42448f172ebcd80f0
T4mmi <thomas.p.janvier@gmail.com>
2018-06-07 11:25:47 +0200

    REL : Added the pre-release tag

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except ImportError:
 
 setup(
     name='itk-binarythinning3d',
-    version='5.0.1',
+    version='5.0.1rc1',
     author='Insight Software Consortium',
     author_email='itk+community@discourse.itk.org',
     packages=['itk'],
